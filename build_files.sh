b1e1d72852e84f6119a33abeb11f6c350f840cc2
narendra patil <narendra.rcpit@gmail.com>
2025-01-18 21:19:55 +0530

    fix error

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,5 @@ echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
+apt-get update
+apt-get install -y libmysqlclient-dev pkg-config
